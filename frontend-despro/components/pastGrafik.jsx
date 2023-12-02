810100fddb4d6647774e1f7e625c4fab45be4833
burjoy <luqman.hadi85@gmail.com>
2023-12-02 17:31:36 +0700

    added component for seeing past event

--- frontend-despro/components/pastGrafik.jsx
+++ frontend-despro/components/pastGrafik.jsx
@@ -2,20 +2,20 @@ import { useEffect, useState } from 'react';
 import { Line } from 'react-chartjs-2';
 import { cobaSnapshot } from '../apis/getFirestore';
 import { initializeApp } from 'firebase/app';
-import { collection, getFirestore, onSnapshot } from 'firebase/firestore';
+import { collection, getFirestore, getDocs } from 'firebase/firestore';
 import { Dropdown } from 'flowbite-react';
 import { Chart } from 'chart.js/auto';
 
 
 function PastGraph() {
   const [tanggal, setTanggal] = useState([]);
+  const [pilihan, setPilihan] = useState("Pilih Tanggal");
   const [chartData, setChartData] = useState({
     labels: ['A', 'B', 'C'],
     datasets: [
       {
         label: 'Keramaian',
         data: [],
-        // backgroundColor: ['#6df74a'],
       },
     ],
     options:{
@@ -30,56 +30,74 @@ function PastGraph() {
   // const [hari_ini, setHariIni] = useState();
   // const [prevDay, setPrevDay] = useState();
 
-  const [currentPop, setCurrentPop] = useState();
-  const [currentDate, setCurrentDate] = useState();
+  const [currentDate, setCurrentDate] = useState("1970-01-01");
+
+  const ambil_data = async(day) => {
+    setPilihan(day);
+    const app = initializeApp(cobaSnapshot());
+    const db = getFirestore(app);
+    try {
+        const querySnapshot = await getDocs(collection(db, "data_mac"));
+        const waktu = [];
+        const orang = [];
+        querySnapshot.forEach((doc) => {
+        //   console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+          const data = doc.data();
+        //   const keys = Object.keys(data);
+        //   console.log(keys);
+          const data_sekarang = data[day];
+          data_sekarang.forEach((individu) => {
+            waktu.push(individu.jam);
+            orang.push(individu.orang);
+          })
+        //   console.log(waktu);
+        //   console.log(orang);
+          setChartData({
+            labels: waktu,
+            datasets: [
+              {
+                label: 'Keramaian',
+                data: orang,
+                // backgroundColor: ['#6df74a'],
+              },
+            ],
+            options:{
+              responsive: true
+            }
+          })
+  
+        //   setCurrentPop(orang[orang.length-1]);
+          setCurrentDate(day);
+        });   
+    } catch (error) {
+        console.log(error);
+    }
+  }
 
   useEffect(() => {
     const app = initializeApp(cobaSnapshot());
     const db = getFirestore(app);
-    const data = collection(db, "data_mac");
-    onSnapshot(data, (snapshot) => {
-      const waktu = [];
-      const orang = [];
-      snapshot.forEach((data_orang) => {
-        const data = data_orang.data();
 
-        const panjang = Object.keys(data).length;
-
-        const keys = Object.keys(data);
-  
-        const sortedKeys = keys.sort();
-
-        setTanggal(sortedKeys);
-
-        const current_data = data[sortedKeys[panjang-1]];
-
-        current_data.forEach((buat_grafik) => {
-          // console.log(buat_grafik)
-          waktu.push(buat_grafik.jam);
-          orang.push(buat_grafik.orang);
-        })
-
-        // console.log(waktu);
-        // console.log(orang);
-
-        setChartData({
-          labels: waktu,
-          datasets: [
-            {
-              label: 'Keramaian',
-              data: orang,
-              // backgroundColor: ['#6df74a'],
-            },
-          ],
-          options:{
-            responsive: true
-          }
-        })
-
-        setCurrentPop(orang[orang.length-1]);
-        setCurrentDate(keys[panjang-1]);
-      })
-    })
+    const fetchData = async () => {
+        try {
+            const querySnapshot = await getDocs(collection(db, "data_mac"));
+            querySnapshot.forEach((doc) => {
+            //   console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+              const data = doc.data();
+              const keys = Object.keys(data);
+            //   console.log(keys);
+    
+              const sortedKeys = keys.sort();
+    
+            //   console.log(sortedKeys);
+              setTanggal(sortedKeys);
+            }
+            );   
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    fetchData();
   }, []);
 
   return (
@@ -87,25 +105,23 @@ function PastGraph() {
     <div className='max-md:h-screen max-md:flex max-md:flex-col max-md:justify-center'>
       <div className='flex justify-center mt-2 max-md:mt-0'>
         <div className="shadow-md rounded p-6 bg-white w-[80%] text-center max-md:w-[100%]">
-          <h2 className="text-xl font-semibold mt-4">{`Tanggal Hari Ini: ${currentDate}`}</h2>
-          <h2 className="text-xl font-semibold mt-4">{`Jumlah orang saat ini: ${currentPop}`}</h2>
+          <h2 className="text-xl font-semibold mt-4">{`Tanggal dipilih: ${currentDate}`}</h2>
           {/* <img className="rounded mx-auto" src="https://picsum.photos/200" alt="Image"></img> */}
           <Line data={chartData}/>
         </div>
       </div>
 
-    <div className='flex justify-center'>
-      <Dropdown label="Dropdown button" dismissOnClick={true} className='mx-auto'>
-        <Dropdown.Item>Dashboard</Dropdown.Item>
-        <Dropdown.Item>Settings</Dropdown.Item>
-        <Dropdown.Item>Sign out</Dropdown.Item>
-        {tanggal?.map((hari, index) => {
-          <Dropdown.Item>{hari}</Dropdown.Item>
-        })}
+    <div className='flex justify-center mt-4'>
+      <Dropdown label={pilihan} dismissOnClick={true} className='mx-auto' placement='top'>
+        {tanggal?.map((hari, index) => (
+          <Dropdown.Item key={index} onClick={() => ambil_data(hari)}>{hari}</Dropdown.Item>
+        ))}
       </Dropdown>
     </div>
-
     </div>
+    {/* {tanggal?.map((hari, index) => (
+          <h2 key={index}>{hari}</h2>
+        ))} */}
       {/* {pastGraphs.map((pastGraph, index) => (
         <div key={index} className='flex justify-center mt-5'>
           <div className="shadow-md rounded p-6 bg-white w-[80%] text-center">
